2844eac4520b9e72a59308e2e3b796c38bb99b66
caitstewart <caitlin.stewart@gsa.gov>
2017-02-22 13:55:44 -0500

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,11 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "fedsim@gsa.gov";
+$subject= "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) 
+echo "Thank You!";
+
+?>
