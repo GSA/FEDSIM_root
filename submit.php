45d6fd6e874892d987208c568a3900056035f55b
caitstewart <caitlin.stewart@gsa.gov>
2017-02-22 14:31:35 -0500

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,6 +1,6 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+<?php $name = $_GET['name'];
+$email = $_GET['email'];
+$message = $_GET['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "fedsim@gsa.gov";
 $subject= "Contact Form";
