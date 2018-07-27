16be3a91f31bad183609743d5f8617c48302fca6
Danielle Hickinbotham <danielle.hickinbotham@gmail.com>
2018-07-27 09:42:43 -0400

    Revert "EXPRESS Removal"
    This reverts commit 976bb4fc07237b515850936a652ce7af244036c7.

---
+++ send.php
@@ -0,0 +1,34 @@
+<?php
+  
+  $to = "fedsim@gsa.gov";
+  
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  
+  $body = stripslashes($_POST['message']);
+  
+  $from = $_POST['email'];
+  $headers = "Reply-To: " . $from;
+  $error_messages = array();
+
+  if( strlen($name) < 1 ) {
+    $error_messages[] = "name required";
+  }
+
+  if( strlen($body) < 1 ) {
+    $error_messages[] = "comment required";
+  }
+
+  if( strlen($from) < 1 ) {
+    $error_messages[] = "email required";
+  }
+
+  if(sizeof($error_messages) == 0 && mail($to, $subject, $body, $headers)) {
+    echo("success");
+  } else {
+    foreach( $error_messages as $error_message ) {
+      echo "$error_message <br />";
+    }
+  }
+
+?>
